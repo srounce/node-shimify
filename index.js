8e6822d8e7d8bdcd120b8034e9957bd153485fe4
Samuel Rounce <me@samuelrounce.co.uk>
2014-02-26 11:52:35 +0000

    Added gitignore Updated to browserify v2 transform

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-var fs = require('fs');
-var shimPath = require.resolve('es5-shim');
-var shimCode = fs.readFileSync(shimPath, 'utf8');
+var fs = require('fs')
+  , through = require('through')
+  , shimPath = require.resolve('es5-shim')
+  , shimCode = fs.readFileSync(shimPath, 'utf8')
+  , applied = false;
 
-module.exports = function (bundle) {
-    bundle.prepend(shimCode);
+module.exports = function (file) {
+  var d = ""
+    , stream = through(write, end);
+
+  return stream;
+
+  function write(buf) {
+    d += buf;
+  }
+
+  function end() {
+    d =  "\"use strict\";\n" + d;
+    if(!applied) {
+      d = shimCode + "\n/* ES5 SHIM END */\n" + d;
+      applied = true;
+    }
+    //console.log(file, "size: ", d.length);
+    stream.queue(d);
+    stream.queue(null);
+  }
 };
